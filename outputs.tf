4648400fe89a2d0bf28ff78d2b51ff8d2d659b86
Agurla venu <142922823+venuagurla19@users.noreply.github.com>
2025-06-03 14:16:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "jenkins_public_ip" {
     description = "Public IP of the Jenkins EC2 instance"
-    value       = aws_instance.jenkins.public_ip
+    value       = aws_instance.jenkins_instance.public_ip
 }
 
 output "s3_bucket_name" {
